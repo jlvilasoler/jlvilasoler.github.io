8167d463c4e9dbed72a8104f6300084fa564bdc1
jlvilasoler <jlvilasoler@hotmail.com>
2023-06-27 21:09:23 -0300

    prueba

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -3,15 +3,21 @@ import "/src/components/ItemCard/ItemCard.scss"
 import { Link } from "react-router-dom";
 
 const ItemCard = ({ id, foto , articulo , descripcion , marca , precio}) => {
+
+
   return (
-    <div className="col-2 m-3">
-      <img src={foto} alt={articulo} />
+    <div className="col-2 m-4">
+      <Link to={`/detail/${id}`}><img src={foto} alt={articulo} /></Link>
+
       <h2>{articulo}</h2>
       <h3>{descripcion}</h3>
       <p>{marca}</p>
       <p className="precio">${precio}</p>
+
+
       <Link className="boton-ver" to={`/detail/${id}`}>Ver más</Link>
-    </div>
+
+      </div>
   );
 };
 
